1388f13658551a9161f11a4e80f045e2b99bf7b3
TPhong <dangthanhphong2209@gmail.com>
2023-06-05 00:41:54 +0700

    Presentational and Container Components

---
+++ FER201m/Exercise9/my-app/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer className='foot'>
+                <nav>
+                    <div>
+                        <p className='highlight'>
+                            <span className='highlight span'>@Copyright2022</span>
+                        </p>
+                    </div>
+                </nav>
+            </footer>
+        )
+    }
+}
