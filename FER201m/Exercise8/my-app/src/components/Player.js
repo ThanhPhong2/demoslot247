2cf80348033552f34f4a104e8c4dc9b368a69d4e
TPhong <dangthanhphong2209@gmail.com>
2023-06-05 00:03:30 +0700

    Functional Components

--- FER201m/Exercise8/my-app/src/components/Player.js
+++ FER201m/Exercise8/my-app/src/components/Player.js
@@ -1,19 +1,19 @@
 import React from 'react'
-import { Players } from '../Share/ListOfPlayers'
-export default function Players {
-  
-    return (
+import { Players as PlayerList } from '../shared/ListOfPlayers'
+
+export default function Players() {
+  return (
     <div className='container'>
-        {Players.map((player)=>(
-           <div className='column'>
-           <div className='card'>
-           <img src={player.img}/>
-             <h3>{player.name}</h3>
-             <p className='title'>{player.club}</p>
-             <p><button>Detail</button></p>
-           </div>
-         </div>
-        ))}
+      {PlayerList.map((player) => (
+        <div className='column'>
+          <div className='card'>
+            <img src={player.img} />
+            <h3>{player.name}</h3>
+            <p className='title'>{player.club}</p>
+            <p><button>Detail</button></p>
+          </div>
+        </div>
+      ))}
     </div>
   )
-        }
+}
